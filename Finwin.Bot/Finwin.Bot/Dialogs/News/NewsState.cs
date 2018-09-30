05745eb2bf4976ffb3d58590d8313031f31574a7
Michael Williams <mwilliams@flusharcade.com>
2018-09-30 10:47:03 +0100

    Building new v4 Sdk news dialog.

---
+++ Finwin.Bot/Finwin.Bot/Dialogs/News/NewsState.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Finwin.Bot.Dialogs
+{
+    /// <summary>
+    /// User state properties for Greeting.
+    /// </summary>
+    public class NewsState
+    {
+    }
+}
--- Finwin.Bot/Finwin.Bot/FinwinBot.cs
+++ Finwin.Bot/Finwin.Bot/FinwinBot.cs
@@ -1,11 +1,22 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+
+using Finwin.Bot;
+using Finwin.Bot.Dialogs;
 
 namespace Finwin.Bot
 {
@@ -14,8 +25,12 @@ namespace Finwin.Bot
     /// </summary>
     public class FinwinBot : IBot
     {
-        private const string WelcomeText = "This bot will introduce you to natural language processing with LUIS. " +
-            "Type an utterance to get started";
+        // Supported LUIS Intents
+        public const string GreetingIntent = "Greeting";
+        public const string CancelIntent = "Cancel";
+        public const string HelpIntent = "Help";
+        public const string NewsIntent = "News";
+        public const string NoneIntent = "None";
 
         /// <summary>
         /// Key in the bot config (.bot file) for the LUIS instance.
@@ -23,84 +38,242 @@ namespace Finwin.Bot
         /// </summary>
         public static readonly string FinwinKey = "BasicBotLuisApplication";
 
-        /// <summary>
-        /// Services configured from the ".bot" file.
-        /// </summary>
+        private readonly IStatePropertyAccessor<NewsState> _greetingStateAccessor;
+        private readonly IStatePropertyAccessor<DialogState> _dialogStateAccessor;
+        private readonly UserState _userState;
+        private readonly ConversationState _conversationState;
         private readonly BotServices _services;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="LuisBot"/> class.
+        /// Initializes a new instance of the <see cref="FinwinBot"/> class.
         /// </summary>
-        /// <param name="services">Services configured from the ".bot" file.</param>
-        public FinwinBot(BotServices services)
+        /// <param name="botServices">Bot services.</param>
+        /// <param name="accessors">Bot State Accessors.</param>
+        public FinwinBot(BotServices services, UserState userState, ConversationState conversationState, ILoggerFactory loggerFactory)
         {
-            _services = services ?? throw new System.ArgumentNullException(nameof(services));
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _userState = userState ?? throw new ArgumentNullException(nameof(userState));
+            _conversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+
+            _greetingStateAccessor = _userState.CreateProperty<NewsState>(nameof(NewsState));
+            _dialogStateAccessor = _conversationState.CreateProperty<DialogState>(nameof(DialogState));
+
+            // Verify LUIS configuration.
             if (!_services.LuisServices.ContainsKey(FinwinKey))
             {
-                throw new System.ArgumentException($"Invalid configuration.  Please check your '.bot' file for a LUIS service named '{FinwinKey}'.");
+                throw new InvalidOperationException($"The bot configuration does not contain a service type of `luis` with the id `{FinwinKey}`.");
             }
+
+            Dialogs = new DialogSet(_dialogStateAccessor);
+            Dialogs.Add(new NewsDialog(_greetingStateAccessor, loggerFactory));
         }
 
+        private DialogSet Dialogs { get; set; }
+
         /// <summary>
-        /// Every conversation turn for our LUIS Bot will call this method.
-        /// There are no dialogs used, the sample only uses "single turn" processing,
-        /// meaning a single request and response, with no stateful conversation.
+        /// Run every turn of the conversation. Handles orchestration of messages.
         /// </summary>
-        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
-        /// for processing this conversation turn. </param>
-        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
-        /// or threads to receive notice of cancellation.</param>
-        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
-        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        /// <param name="turnContext">Bot Turn Context.</param>
+        /// <param name="cancellationToken">Task CancellationToken.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken)
         {
-            if (turnContext.Activity.Type == ActivityTypes.Message)
+            var activity = turnContext.Activity;
+
+            // Create a dialog context
+            var dc = await Dialogs.CreateContextAsync(turnContext);
+
+            if (activity.Type == ActivityTypes.Message)
             {
-                // Check LUIS model
-                var recognizerResult = await _services.LuisServices[FinwinKey].RecognizeAsync(turnContext, cancellationToken);
-                var topIntent = recognizerResult?.GetTopScoringIntent();
-                if (topIntent != null && topIntent.HasValue && topIntent.Value.intent != "None")
+                // Perform a call to LUIS to retrieve results for the current activity message.
+                var luisResults = await _services.LuisServices[FinwinKey].RecognizeAsync(dc.Context, cancellationToken).ConfigureAwait(false);
+
+                // If any entities were updated, treat as interruption.
+                // For example, "no my name is tony" will manifest as an update of the name to be "tony".
+                var topScoringIntent = luisResults?.GetTopScoringIntent();
+
+                var topIntent = topScoringIntent.Value.intent;
+
+                // update greeting state with any entities captured
+                await UpdateGreetingState(luisResults, dc.Context);
+
+                // Handle conversation interrupts first.
+                var interrupted = await IsTurnInterruptedAsync(dc, topIntent);
+                if (interrupted)
                 {
-                    await turnContext.SendActivityAsync($"==>LUIS Top Scoring Intent: {topIntent.Value.intent}, Score: {topIntent.Value.score}\n");
+                    // Bypass the dialog.
+                    // Save state before the next turn.
+                    await _conversationState.SaveChangesAsync(turnContext);
+                    await _userState.SaveChangesAsync(turnContext);
+                    return;
+                }
+
+                // Continue the current dialog
+                var dialogResult = await dc.ContinueDialogAsync();
+
+                // if no one has responded,
+                if (!dc.Context.Responded)
+                {
+                    // examine results from active dialog
+                    switch (dialogResult.Status)
+                    {
+                        case DialogTurnStatus.Empty:
+                            switch (topIntent)
+                            {
+                                case GreetingIntent:
+                                    //await dc.BeginDialogAsync(nameof(NewsDialog));
+                                    break;
+
+                                case NewsIntent:
+                                    await dc.BeginDialogAsync(nameof(NewsDialog));
+                                    break;
+
+                                case NoneIntent:
+                                default:
+                                    // Help or no intent identified, either way, let's provide some help.
+                                    // to the user
+                                    await dc.Context.SendActivityAsync("I didn't understand what you just said to me.");
+                                    break;
+                            }
+
+                            break;
+
+                        case DialogTurnStatus.Waiting:
+                            // The active dialog is waiting for a response from the user, so do nothing.
+                            break;
+
+                        case DialogTurnStatus.Complete:
+                            await dc.EndDialogAsync();
+                            break;
+
+                        default:
+                            await dc.CancelAllDialogsAsync();
+                            break;
+                    }
+                }
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (activity.MembersAdded.Any())
+                {
+                    // Iterate over all new members added to the conversation.
+                    foreach (var member in activity.MembersAdded)
+                    {
+                        // Greet anyone that was not the target (recipient) of this message.
+                        // To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards for more details.
+                        if (member.Id != activity.Recipient.Id)
+                        {
+                            var welcomeCard = CreateAdaptiveCardAttachment();
+                            var response = CreateResponse(activity, welcomeCard);
+                            await dc.Context.SendActivityAsync(response).ConfigureAwait(false);
+                        }
+                    }
+                }
+            }
+
+            await _conversationState.SaveChangesAsync(turnContext);
+            await _userState.SaveChangesAsync(turnContext);
+        }
+
+        // Determine if an interruption has occured before we dispatch to any active dialog.
+        private async Task<bool> IsTurnInterruptedAsync(DialogContext dc, string topIntent)
+        {
+            // See if there are any conversation interrupts we need to handle.
+            if (topIntent.Equals(CancelIntent))
+            {
+                if (dc.ActiveDialog != null)
+                {
+                    await dc.CancelAllDialogsAsync();
+                    await dc.Context.SendActivityAsync("Ok. I've cancelled our last activity.");
                 }
                 else
                 {
-                    var msg = @"No LUIS intents were found.
-                            This sample is about identifying two user intents:
-                            'Calendar.Add'
-                            'Calendar.Find'
-                            Try typing 'Add Event' or 'Show me tomorrow'.";
-                    await turnContext.SendActivityAsync(msg);
+                    await dc.Context.SendActivityAsync("I don't have anything to cancel.");
                 }
+
+                return true;        // Handled the interrupt.
             }
-            else if (turnContext.Activity.Type == ActivityTypes.ConversationUpdate)
+
+            if (topIntent.Equals(HelpIntent))
             {
-                // Send a welcome message to the user and tell them what actions they may perform to use this bot
-                await SendWelcomeMessageAsync(turnContext, cancellationToken);
+                await dc.Context.SendActivityAsync("Let me try to provide some help.");
+                await dc.Context.SendActivityAsync("I understand greetings, being asked for help, or being asked to cancel what I am doing.");
+                if (dc.ActiveDialog != null)
+                {
+                    await dc.RepromptDialogAsync();
+                }
+
+                return true;        // Handled the interrupt.
             }
-            else
+
+            return false;           // Did not handle the interrupt.
+        }
+
+        // Create an attachment message response.
+        private Activity CreateResponse(Activity activity, Attachment attachment)
+        {
+            var response = activity.CreateReply();
+            response.Attachments = new List<Attachment>() { attachment };
+            return response;
+        }
+
+        // Load attachment from file.
+        private Attachment CreateAdaptiveCardAttachment()
+        {
+            var adaptiveCard = File.ReadAllText(@".\Dialogs\Welcome\Resources\welcomeCard.json");
+            return new Attachment()
             {
-                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected", cancellationToken: cancellationToken);
-            }
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCard),
+            };
         }
 
         /// <summary>
-        /// On a conversation update activity sent to the bot, the bot will
-        /// send a message to the any new user(s) that were added.
+        /// Helper function to update greeting state with entities returned by LUIS.
         /// </summary>
-        /// <param name="turnContext">Provides the <see cref="ITurnContext"/> for the turn of the bot.</param>
-        /// <param name="cancellationToken" >(Optional) A <see cref="CancellationToken"/> that can be used by other objects
-        /// or threads to receive notice of cancellation.</param>
-        /// <returns>>A <see cref="Task"/> representing the operation result of the Turn operation.</returns>
-        private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        /// <param name="luisResult">LUIS recognizer <see cref="RecognizerResult"/>.</param>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        private async Task UpdateGreetingState(RecognizerResult luisResult, ITurnContext turnContext)
         {
-            foreach (var member in turnContext.Activity.MembersAdded)
+            if (luisResult.Entities != null && luisResult.Entities.HasValues)
             {
-                if (member.Id != turnContext.Activity.Recipient.Id)
+                // Get latest GreetingState
+                var greetingState = await _greetingStateAccessor.GetAsync(turnContext, () => new NewsState());
+                var entities = luisResult.Entities;
+
+                // Supported LUIS Entities
+                string[] userNameEntities = { "userName", "userName_patternAny" };
+                string[] userLocationEntities = { "userLocation", "userLocation_patternAny" };
+
+                // Update any entities
+                // Note: Consider a confirm dialog, instead of just updating.
+                foreach (var name in userNameEntities)
                 {
-                    await turnContext.SendActivityAsync(
-                        $"Welcome to LuisBot {member.Name}. {WelcomeText}",
-                        cancellationToken: cancellationToken);
+                    // Check if we found valid slot values in entities returned from LUIS.
+                    if (entities[name] != null)
+                    {
+                        // Capitalize and set new user name.
+                        var newName = (string)entities[name][0];
+                        //greetingState.Name = char.ToUpper(newName[0]) + newName.Substring(1);
+                        break;
+                    }
                 }
+
+                foreach (var city in userLocationEntities)
+                {
+                    if (entities[city] != null)
+                    {
+                        // Captilize and set new city.
+                        var newCity = (string)entities[city][0];
+                        //greetingState.City = char.ToUpper(newCity[0]) + newCity.Substring(1);
+                        break;
+                    }
+                }
+
+                // Set the new values into state.
+                await _greetingStateAccessor.SetAsync(turnContext, greetingState);
             }
         }
     }
